85553c20be9d38afa0606c76011ec3339731d78d
Felipe <felipe.dspereira@gmail.com>
2022-01-21 21:19:32 -0400

    removes cacheable

--- src/main/java/com/felipe/feign/client/rest/client/HelloClient.java
+++ src/main/java/com/felipe/feign/client/rest/client/HelloClient.java
@@ -1,13 +1,11 @@
 package com.felipe.feign.client.rest.client;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.felipe.feign.client.rest.client.configuration.HelloConfiguration;
 
 @FeignClient(name = "hello", url = "http://localhost:8080", configuration = HelloConfiguration.class)
-@Cacheable(cacheNames = "teste")
 public interface HelloClient {
 
   @GetMapping("/test")
